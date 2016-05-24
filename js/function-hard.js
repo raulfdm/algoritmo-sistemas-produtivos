cc42f43ebaec8fddcc0203b9e52c2dff64cdd695
Raul F. de Melo <raulfdm@hotmail.com>
2016-05-24 18:54:48 -0300

    Modularização do código e outros
    • Separado o js de cada complexidade, unificando os códigos semelhantes;
    • modularização das partes do html em includes;
    • inicio da alteração dos modais de cada tabela (em andamento)

---
+++ js/function-hard.js
@@ -0,0 +1,61 @@
+function calculate(sequence) {
+    var condicoesMirror = condicoes;
+    var f1 = condicoesMirror.fabrica1;
+    var f2 = condicoesMirror.fabrica2;
+    //Um array espelho para retornar.
+    var getArray = sequence.toString();
+    //Variável do result
+    var result = 0;
+    //Posição Atual
+    var currentPos = 0;
+    //Posição Anterior
+    var prevPos = 0;
+    //Sinalizador de fim do array
+    var stop = true;
+    var valPosition = 0;
+    //Começando o trabalho
+    while (stop) {
+        //valida se o array ainda tem elementos
+        if (sequence.length === 0) {
+            stop = false;
+        }
+        //Atribui o primeiro elemento da sequence ao Atual
+        currentPos = sequence[0];
+        //console.log('currentPos: '+currentPos);
+        //Valida se o array já chegou ao final
+        //console.log('fabrica1: ' + f1 + '\nvalores[currentPos]: ' + valores[currentPos] + '\nfabrica2: ' + f2);
+
+        if (f1 - condicoesMirror.valores[currentPos] > 0) {
+            f1 -= condicoesMirror.valores[currentPos];
+        } else if (f2 - condicoesMirror.valores[currentPos] > 0) {
+            f2 -= condicoesMirror.valores[currentPos];
+        } else if (condicoesMirror.valores[currentPos] === undefined) {
+            console.log("fim");
+        } else {
+            console.log("Sem capacidade");
+        }
+        if (currentPos === undefined) {
+            currentPos = 0;
+        }
+        //pega o valor correspondente e soma no result
+        valPosition = A1.get[prevPos][currentPos];
+        //console.log('prevPos: '+prevPos +'\ncurrentPos: '+currentPos+"\nvalPosition: "+valPosition);
+        result += valPosition;
+        //Imprime o array
+        //console.log("currentPos " + currentPos + " | " + "prevPos" + prevPos);
+        //Salva o número atual
+        prevPos = currentPos;
+        //Remove o elemento do array
+        currentIndex = sequence.indexOf(currentPos);
+        sequence.splice(currentIndex, 1);
+    }
+    //Cria um objeto com o array que foi utilizado e o valor da conta
+    var obj = {
+        array: getArray,
+        result: result.toFixed(0),
+        fabrica1: f1,
+        fabrica2: f2
+    };
+    //Retorna o objeto
+    return obj;
+}
--- js/main-function.js
+++ js/main-function.js
@@ -1,62 +1,16 @@
-    //Chama a api
-    jsdk.getAPI('maths');
+//Chama a api
+jsdk.getAPI('maths');
 
-    //Cria a matriz usando a api
-    function mountArray(array, dim) {
-        var arrayDone = mathsAPI.getInstance('masfufa', {
-            data: array, //dados a serem usados
-            dim: dimensaoArray //tamanho da matriz
-        });
+//Cria a matriz usando a api
+function mountArray(array, dim) {
+    var arrayDone = mathsAPI.getInstance('masfufa', {
+        data: array, //dados a serem usados
+        dim: dimensaoArray //tamanho da matriz
+    });
 
-        return arrayDone;
-    }
+    return arrayDone;
+}
 $(document).ready(function() {
-    //Função que faz o calculo
-    function calculate(sequence) {
-        //Um array espelho para retornar.
-        var getArray = sequence.toString();
-        //Variável do result
-        var result = 0;
-        //Posição Atual
-        var currentPos = 0;
-        //Posição Anterior
-        var prevPos = 0;
-        //Sinalizador de fim do array
-        var stop = true;
-        var valPosition = 0;
-        //Começando o trabalho
-        while (stop) {
-            //valida se o array ainda tem elementos
-            if (sequence.length === 0) {
-                stop = false;
-            }
-            //Atribui o primeiro elemento da sequence ao Atual
-            currentPos = sequence[0];
-            //console.log('currentPos: '+currentPos);
-            //Valida se o array já chegou ao final
-            if (currentPos === undefined) {
-                currentPos = 0;
-            }
-            //pega o valor correspondente e soma no result
-            valPosition = A1.get[prevPos][currentPos];
-            //console.log('prevPos: '+prevPos +'\ncurrentPos: '+currentPos+"\nvalPosition: "+valPosition);
-            result += valPosition;
-            //Imprime o array
-            //console.log("currentPos " + currentPos + " | " + "prevPos" + prevPos);
-            //Salva o número atual
-            prevPos = currentPos;
-            //Remove o elemento do array
-            currentIndex = sequence.indexOf(currentPos);
-            sequence.splice(currentIndex, 1);
-        }
-        //Cria um objeto com o array que foi utilizado e o valor da conta
-        var obj = {
-            array: getArray,
-            result: result.toFixed(0)
-        };
-        //Retorna o objeto
-        return obj;
-    }
 
     //Preenchimento Randomico
     function fillArrayRandom() {
@@ -110,6 +64,9 @@ $(document).ready(function() {
 
     $('#randomTimesFunc').on('click', function() {
         var numberTimes = $('#randomTimes').val();
+        if (!validaNumero(numberTimes)) {
+            return;
+        }
         var start = time();
         cleanResults();
         var total = [];
@@ -147,7 +104,7 @@ $(document).ready(function() {
     function cleanResults() {
         $('.result-list li').remove();
         $('.result-list').removeClass("alert alert-danger");
-        $('.result-time p').text("");
+        $('.result-time p').text("0.00 seconds | 0 milliseconds wasted");
     }
 
     function time() {
@@ -156,5 +113,15 @@ $(document).ready(function() {
 
     function printTime(ini, end) {
         $('.result-time p').text(((end - ini) / 1000).toFixed(2) + " seconds wasted");
+    }
+
+    function validaNumero(numberTimes) {
+        if (numberTimes == undefined || numberTimes == "" || numberTimes == 0 || numberTimes.length == 0 || numberTimes < 0) {
+            cleanResults();
+            $('.result-list').append("<li>Digite um valor válido</li>").addClass("alert alert-danger");
+            return false;
+        } else {
+            return true;
+        }
     }
 });
